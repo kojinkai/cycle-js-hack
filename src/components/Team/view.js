50ee20a4d92ff556900787f7c96d4a7459b87b3d
Lewis Nixon <lewcfn@gmail.com>
2017-12-14 18:00:41 +0100

    reafactor(Team): map data and return JSX for easier templating

--- src/components/Team/view.js
+++ src/components/Team/view.js
@@ -1,19 +1,16 @@
 export default function view(state$) {
   // data here is an Immutable List
   // exported by intent.js
-  const getFieldValueFromName = data => name => data
-    .find(field => field.get('name') === name)
-    .get('value');
+  const getTeamEntryFromData = data => data
+    .map(field => (<p>{field.get('name')}: <span>{field.get('value')}</span></p>))
+    .toJS();
 
   return state$
     .map(props => {
-      const populateDisplayField = getFieldValueFromName(props.teamData);
-
+      const { teamData } = props;
       return (
         <div className="team">
-          <p>Player One: <span>{populateDisplayField('playerOne')}</span></p>
-          <p>Player Two: <span>{populateDisplayField('playerTwo')}</span></p>
-          <p>Team: <span>{populateDisplayField('team')}</span></p>
+          { getTeamEntryFromData(teamData) }
         </div>
       );
     });
